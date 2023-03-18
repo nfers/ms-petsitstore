1510a1fb4102d3adbb68e4a8f6a80b3ce61d5c5b
Nay Ferreira <nchristina@ciandt.com>
2023-03-18 17:54:44 -0300

    criação inicial do projeto

---
+++ src/main/java/br/com/mspetsitstore/MsPetsitstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.mspetsitstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPetsitstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPetsitstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mspetsitstore/MsPetsitstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mspetsitstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPetsitstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
